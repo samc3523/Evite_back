1fa2cd46e00eeac840072a5c2a9d0bf129897a93
Samuel Crockford <crockfords22@icloud.com>
2023-07-17 17:10:11 -0400

    second test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use] extern crate rocket;
+use rocket::serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct BlogPost {
+    id: i32,
+    title: String,
+    body: String,
+    published: bool,
+}
+
+
+#[get("/")]
+fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    let rocket = rocket::build();
+    rocket.mount("/", routes![index])
 }
